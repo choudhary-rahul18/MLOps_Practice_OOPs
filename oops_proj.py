5cd41bc7a9ab55e4aa306a84e05d9ee8bd9b8470
Rahul Choudhary <rahulchoudhary@Rahuls-MacBook-Air-5.local>
2026-02-04 03:55:10 +0530

    Signup and Signin feature add

--- oops_proj.py
+++ oops_proj.py
@@ -15,16 +15,39 @@ class Chatbook:
                             "5. Press any other key to exit \n"))
         
         if user_input == 1:
-            pass
+            self.signup()
         elif user_input ==2:
-            pass
+            self.signin()
         elif user_input == 3:
             pass
         elif user_input == 4:
             pass
-        elif user_input == 5:
-            pass
         else:
             exit()
+
+    def signup(self):
+        email = input("Enter your email here --> ")
+        pwd = input("Setup your password here --> ")
+        self.username = email
+        self.password = pwd
+        print("You have signup successfully !!\n")
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("Please Signedup first by pressing 1 in the main menu\n")
+        
+        else:
+            email = input("Enter your email here --> ")
+            pwd = input("Setup your password here --> ")
+
+            if self.username == email and self.password == pwd:
+                print("Signin Suceesfully!!\n")
+                self.loggedin = True
+            else:
+                print("Incorrect username or password\n")
+        
+        self.menu()
+
 
 ram = Chatbook()
